8f2f5c4e70147c690fd1418e028af5348c622826
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-01-29 14:40:27 +0800

    add EditComp.js

---
+++ src/Component/Class/EditComp.jsx
@@ -0,0 +1,127 @@
+import axios from "axios";
+import e from "cors";
+import React, { Component } from "react";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+import qs from "querystring";
+import { useLocation } from "react-router-dom";
+
+const api = "http://localhost:3001";
+class EditComp extends Component {
+  constructor(props) {
+    super(props);
+
+    const location = useLocation;
+    const { id_mahasiswa, nim, nama, jurusan } = location.state;
+    this.state = {
+      id_mahasiswa: id_mahasiswa,
+      nim: nim,
+      nama: nama,
+      jurusan: jurusan,
+      response: "",
+      display: "none",
+    };
+  }
+
+  handleChange = () => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  ubahMahasiswa = (idmahasiswa) => {
+    const data = qs.stringify({
+      id_mahasiswa: this.state.id_mahasiswa,
+      nim: this.state.nim,
+      nama: this.state.nama,
+      jurusan: this.state.jurusan,
+    });
+
+    axios.put(api + "/ubah", data).then((json) => {
+      if (json === 200) {
+        this.setState({
+          response: json.data.values,
+          display: "block",
+        });
+      } else {
+        this.setState({
+          response: json.data.values,
+          display: "block",
+        });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <h4 className="mt-3">Form Tambah Data</h4>
+          <Alert color="success" style={{ display: this.state.display }}>
+            {this.state.response}
+          </Alert>
+          <Form className="form">
+            <Col>
+              <Label>NIM</Label>
+              <FormGroup>
+                <Row>
+                  <Col>
+                    <Input
+                      type="text"
+                      name="nim"
+                      value={this.state.nim}
+                      onChange={this.handleChange}
+                      placeholder="Masukkan NIM"
+                    />
+                  </Col>
+                </Row>
+              </FormGroup>
+              <Label>Nama</Label>
+              <FormGroup>
+                <Row>
+                  <Col>
+                    <Input
+                      type="text"
+                      name="nama"
+                      value={this.state.nama}
+                      onChange={this.handleChange}
+                      placeholder="Masukkan Nama"
+                    />
+                  </Col>
+                </Row>
+              </FormGroup>
+              <Label>Jurusan</Label>
+              <FormGroup>
+                <Row>
+                  <Col>
+                    <Input
+                      type="text"
+                      name="jurusan"
+                      value={this.state.jurusan}
+                      onChange={this.handleChange}
+                      placeholder="Masukkan Jurusan"
+                    />
+                  </Col>
+                </Row>
+              </FormGroup>
+              <FormGroup>
+                <Button type="button" onClick={this.addMahasiswa}>
+                  Update
+                </Button>
+              </FormGroup>
+            </Col>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default EditComp;
