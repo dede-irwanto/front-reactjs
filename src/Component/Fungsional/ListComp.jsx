3406bce455ced0b7087596e06fa1485064bc40c7
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-01-30 11:26:47 +0800

    add ListComp.jsx function

---
+++ src/Component/Fungsional/ListComp.jsx
@@ -0,0 +1,73 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, NavLink, Button, Table } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const api = "http://localhost:3001";
+
+const ListComp = () => {
+  const [mahasiswa, setMahasiswa] = useState([]);
+
+  useEffect(() => {
+    fectData();
+  }, []);
+
+  //function "fetchData"
+  const fectData = async () => {
+    //fetching
+    const response = await axios.get(api + "/tampil");
+    //get response data
+    const data = await response.data.values;
+
+    //assign response data to state "posts"
+    setMahasiswa(data);
+  };
+
+  return (
+    <div>
+      <Container>
+        <h2>Data Mahasiswa</h2>
+        <NavLink href="/mahasiswa/tambah">
+          <Button color="primary">Tambah Data</Button>
+        </NavLink>
+        <hr />
+        <Table className="table-bordered">
+          <thead>
+            <tr>
+              <th>NIM</th>
+              <th>Nama</th>
+              <th>Jurusan</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mahasiswa.map((mahasiswa) => (
+              <tr key={mahasiswa.id_mahasiswa}>
+                <td>{mahasiswa.nim}</td>
+                <td>{mahasiswa.nama}</td>
+                <td>{mahasiswa.jurusan}</td>
+                <td>
+                  <Link
+                    to={{
+                      pathname: `/mahasiswa/edit/${mahasiswa.id_mahasiswa}`,
+                    }}
+                    state={{
+                      id_mahasiswa: mahasiswa.id_mahasiswa,
+                      nim: mahasiswa.nim,
+                      nama: mahasiswa.nama,
+                      jurusan: mahasiswa.jurusan,
+                    }}
+                  >
+                    <Button>Edit</Button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
+
+export default ListComp;
